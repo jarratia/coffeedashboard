8440dcf2c43efcbd5fa56e0d84bfd2e160321e6c
jarratia <jarratia34@gmail.com>
2020-06-20 15:44:43 -0400

    subiendo

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,21 @@
 class DashboardController < ApplicationController
   def index
+    #ventas por mes
     @sales_grouped_by_month = CoffeSale.group("strftime('%m-%Y', date_time)").order(date_time: :asc).order(created_at: :asc).sum(:amount)
 
+    #Cantidad de ventas por mes
     @quantity_sales_by_month = CoffeSale.group("strftime('%m-%Y', date_time)").order(date_time: :asc).order(created_at: :asc).count
+
+    @twelve_month_origin = CoffeSale.group_by_month(:date_time, last: 12).order(:origin).count
+  
+    @six_month_origin = CoffeSale.group_by_month(:date_time, last: 6).order(:origin).count
+
+    @three_month_origin = CoffeSale.group_by_month(:date_time, last: 3).order(:origin).count
+
+    @one_month_origin = CoffeSale.group_by_month(:date_time, last: 1).order(:origin).count
+
+    #Ventas por nombre
+    @twelve_month_blend_name = CoffeSale.group(:blend_name).order(:date_time, :asc).count
   end
 
   
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 1000.times do
-    CoffeSale.create(blend_name: Faker::Coffee.blend_name, amount: rand(1990..5490), date_time: Faker::Date.between(3.year.ago, Date.today))
+    CoffeSale.create(blend_name: Faker::Coffee.blend_name, amount: rand(1990..5490), date_time: Faker::Date.between(3.year.ago, Date.today), origin: Faker::Coffee.origin)
 end
